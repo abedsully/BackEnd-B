7cbc2b28ace589d21b8478eaf75589518d398ed2
abedsully <abedsully2909@gmail.com>
2023-11-28 20:56:04 +0700

    pert2

--- pert2/routes/web.php
+++ pert2/routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcomes');
+    return view('welcome');
 });
 
+
+// Ini untuk index
 Route::get('/index', function () {
     return view('index');
 });
